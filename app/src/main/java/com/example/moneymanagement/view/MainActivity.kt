431083dc66237bce795eeb80a3bdb86a59efa1ca
anphuc2005 <phananphuc2005@gmail.com>
2025-09-23 09:08:28 +0700

    init project

--- app/src/main/java/com/example/moneymanagement/view/MainActivity.kt
+++ app/src/main/java/com/example/moneymanagement/view/MainActivity.kt
@@ -1,10 +1,13 @@
-package com.example.moneymanagement
+package com.example.moneymanagement.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.moneymanagement.R
+import com.example.moneymanagement.view.fragment_login.LoginFragment
+import com.example.moneymanagement.view.fragment_login.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, RegisterFragment())
+                .commit()
+        }
+
     }
 }
